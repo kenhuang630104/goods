d831dd5452f1d94a1c8f9bc3d5ae95bb8c1f46c1
ken huang <ken.huang@eten.com.tw>
2018-10-16 17:36:18 +0800

    main function

--- goods.py
+++ goods.py
@@ -1,36 +1,54 @@
+
 import os
 
-
-goods = []
-if os.path.isfile('goods.csv'):
-    print('yeah!!found the file!!')
-    with open('goods.csv', 'r', encoding='utf-8') as f:
+#讀取檔案
+def read_file(filename):
+    goods = []
+    with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
             if '商品,價格' in line:
                 continue
             name, price = line.strip().split(',')
             goods.append([name, price])
-    print(goods)
-else:
-    print('not found!!')
+    return goods
 
-while True:
-	name = input('please input the product name:  or \'q\'for exit!!')
-	if name == 'q':
-		break
-	price = input('please input the product price:')
-	goods.append([name, price])
-print(goods)
+
+#輸入資料
+def user_input(goods):
+    while True:
+        name = input('please input the product name:  or \'q\'for exit!!')
+        if name == 'q':
+            break
+        price = input('please input the product price:')
+        goods.append([name, price])
+    print(goods)
+    return goods
 #print(goods[0][1])#第1個大清單中的第2個欄位
 
-for p in goods:
-	print(p)
+
+#列出紀錄
+def print_goods(goods):
+    for p in goods:
+        print(p)
 #	print(p[0], '的價格是', p[1])
 
-with open('goods.csv', 'w', encoding = 'utf-8') as f:
-	f.write('商品,價格 \n')
-	for p in goods:
-		f.write(p[0] + ',' + p[1] + '\n')
+
+#寫入檔案
+def write_file(filename, goods):
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        f.write('商品,價格 \n')
+        for p in goods:
+            f.write(p[0] + ',' + p[1] + '\n')
+def main():
+    filename = 'goods.csv'
+    if os.path.isfile(filename):
+        print('yeah!!found the file!!')
+        goods = read_file(filename)
+    else:
+        print('not found!!')
+    goods = user_input(goods)
+    print_goods(goods)
+    write_file('goods.csv', goods)
 
 
-
+main()
