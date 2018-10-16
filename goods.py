520a80fbcfd015652b3b3b6db56189d246238493
ken huang <ken.huang@eten.com.tw>
2018-10-16 09:35:27 +0800

    write file

--- goods.py
+++ goods.py
@@ -12,4 +12,7 @@ for p in goods:
 	print(p)
 	print(p[0], '的價格是', p[1])
 
-
+with open('goods.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品,價格 \n')
+	for p in goods:
+		f.write(p[0] + ',' + p[1] + '\n')
